8560180b46d1bd7a026d115cd607e441beaa91f2
IniArthika <arthikasalsabila@gmail.com>
2024-11-06 14:15:02 +0700

    fix

--- crud/form.php
+++ crud/form.php
@@ -49,7 +49,7 @@
                         </td>
                         <td>
                             <select name="kategori_id" id="">
-                                <option value="0">--Pilih Kategori Id--</option>
+                                <option value="0">--Pilih Kategori--</option>
                                 <option value="1">1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
